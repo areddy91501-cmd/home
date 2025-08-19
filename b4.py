323e9d179fc1515491bfd450f57b7a1f9805ca60
areddy91501-cmd <areddy91501@gmail.com>
2025-08-19 10:19:46 +0530

    Add files via upload

---
+++ b4.py
@@ -0,0 +1,7 @@
+name=input("enter the name")
+age=input("enter the age")
+if age >=18:
+ print(name,"is elible to apply for driving license")
+else:
+ print(name,"is not elible to apply for driving license")
+ 
